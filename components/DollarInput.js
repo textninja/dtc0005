1343595a068eb21889abcd0f71a87513c2e09803
Joe Taylor <joe@textninja.net>
2022-01-03 20:06:59 -0800

    fraught attempt number 1

--- components/DollarInput.js
+++ components/DollarInput.js
@@ -1,8 +1,22 @@
 import React, { useState, useRef } from 'react';
 import { TextInput } from 'react-native';
+import styles from '../styles';
+
+function moneyFormat(input) {
+    let amt = parseInt(input.replace(/[^0-9.]/g, ""), 10);
+    if (isNaN(amt)) amt = 0;
+    return "$" + amt.toFixed(2);
+}
 
 export default function DollarInput(props) {
+    const [val, setVal] = useState("0");
+    const shownValue = moneyFormat(val);
+
     return (
-        <TextInput {...props} />
+        <TextInput
+            style={styles.bigText}
+            {...{...props, value: shownValue}}
+            onChangeText={t => setVal(t)}
+        />
     );
 }
--- styles.js
+++ styles.js
@@ -5,5 +5,9 @@ export default StyleSheet.create({
         width: "100%",
         height: "100%",
         padding: 50
+    },
+
+    bigText: {
+        fontSize: 48
     }
 });
