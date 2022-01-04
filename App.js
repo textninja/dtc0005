3a85b52263aeccee8111a0340b80e7abb3057cd7
Joe Taylor <joe@textninja.net>
2022-01-03 19:08:33 -0800

    Added the simple container based implementation

--- App.js
+++ App.js
@@ -1,17 +1,54 @@
-import React, { useState } from 'react';
+import React, { useState, useRef } from 'react';
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, TouchableOpacity, Text, TextInput, View } from 'react-native';
 import DollarInput from './components/DollarInput';
+
+const textStyle = {
+  fontSize: 32
+};
 
 export default function App() {
 
   let [unfocus, setUnfocus] = useState(1);
 
-  return (
-    <View style={{ padding: 50, height: "100%" }} onTouchStart={(e) => setUnfocus(f => f+1)}>
-      <DollarInput
-        unfocus={unfocus}
-        defaultValue="Hello"/>
-    </View>
-  );
+  const [val, setVal] = useState("");
+
+  const textInput = useRef();
+
+  let children = [
+    <Text key="dollar" style={textStyle}>$</Text>,
+    <TextInput
+      key="input"
+      style={textStyle}
+      ref={textInput}
+      defaultValue=""
+      maxLength={5}
+      keyboardType="numeric"
+    />,
+    <Text key="dot-zero-zero" style={textStyle}>.00</Text>
+  ];
+
+  return <View style={{padding: 50, height: "100%" }}>
+    <TouchableOpacity
+      style={{
+        borderWidth: 2,
+        minWidth: 100,
+        borderColor: "black",
+        display: "flex",
+        flexDirection: "row",
+        padding: 5,
+      }}
+      onPress={() => textInput.current.focus()}
+    >
+      {children}
+    </TouchableOpacity>
+  </View>
+
+  // return (
+  //   <View style={{ padding: 50, height: "100%" }} onTouchStart={(e) => setUnfocus(f => f+1)}>
+  //     <DollarInput
+  //       unfocus={unfocus}
+  //       defaultValue="Hello"/>
+  //   </View>
+  // );
 }
